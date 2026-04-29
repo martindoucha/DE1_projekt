ba4aca74a9f169103f6b1ce57ad3d09a739e99b4
KocourekJ27 <269855@vutbr.cz>
2026-04-29 12:24:34 +0200

    clk_en.vhd

--- alarm_clock/alarm_clock.srcs/sources_1/imports/Downloads/clk_en.vhd
+++ alarm_clock/alarm_clock.srcs/sources_1/imports/Downloads/clk_en.vhd
@@ -5,32 +5,38 @@ entity clk_en is
     generic(
         G_MAX : positive := 5 -- Default value
     );
-    Port ( clk : in STD_LOGIC;
-           rst : in STD_LOGIC;
-           ce : out STD_LOGIC);
+    Port ( clk   : in  STD_LOGIC;
+           rst   : in  STD_LOGIC;
+           ce    : out STD_LOGIC;
+           f_out : out STD_LOGIC -- PŘIDANÝ PORT PRO BLIKÁNÍ
+    );
 end clk_en;
 
 architecture Behavioral of clk_en is
-    signal s_cnt : integer range 0 to G_MAX-1;
+    signal s_cnt   : integer range 0 to G_MAX-1;
+    signal s_f_out : std_logic := '0'; -- Vnitřní signál pro překlápění
 begin
 
--- Count clock pulses and generate a one-clock-cycle enable pulse
+    -- Průběžné přiřazení vnitřního stavu na výstupní port
+    f_out <= s_f_out;
+
+    -- Count clock pulses and generate a one-clock-cycle enable pulse
     process (clk) is
     begin
         if rising_edge(clk) then  -- Synchronous process
             if rst = '1' then     -- High-active reset
                 ce      <= '0';   -- Reset output
-                s_cnt <= 0;     -- Reset internal counter
+                s_cnt   <= 0;     -- Reset internal counter
+                s_f_out <= '0';   -- Reset blikání
 
             elsif s_cnt = G_MAX-1 then
-                -- TODO: Set output pulse and reset internal counter
-                ce <= '1';
-                s_cnt <= 0;
+                ce      <= '1';
+                s_cnt   <= 0;
+                s_f_out <= not s_f_out; -- Překlopení stavu f_out
                 
             else
-                -- TODO: Clear output and increment internal counter
-                ce <= '0';
-                s_cnt <= s_cnt + 1;
+                ce      <= '0';
+                s_cnt   <= s_cnt + 1;
                 
             end if;  -- End if for reset/check
         end if;      -- End if for rising_edge
